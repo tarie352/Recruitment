d98a334f9ced752d656f9017a00d20af229015c3
tarie352 <92187204+tarie352@users.noreply.github.com>
2024-04-12 22:51:20 +0000

    Update candidatesPage.php

--- candidatesPage.php
+++ candidatesPage.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Candidates Page</title>
-    <link rel="stylesheet" href="../css/jobsPage.css" />
+    <link rel="stylesheet" href="jobsPage.css" />
 </head>
 
 <body>
@@ -15,7 +15,7 @@
     <main>
         <div class="candidate-listings">
             <?php
-            include("../functions/candidate_fuxn.php");
+            include("candidate_fuxn.php");
 
             // Close the database connection
             mysqli_close($con);
@@ -24,9 +24,9 @@
     </main>
 
     <div class="button-container">
-        <button type="button" onclick="window.location.href = '../view/main.php';" style="margin-top:30px;margin-left:40px;">Main Page</button>
-        <button type="button" onclick="window.location.href = '../view/candidatesPage.php';" style="margin-top:30px;margin-left:40px;">Candidates Page</button>
-        <button type="button" onclick="window.location.href = '../actions/logout.php';">Log Out</button>
+        <button type="button" onclick="window.location.href = 'main.php';" style="margin-top:30px;margin-left:40px;">Main Page</button>
+        <button type="button" onclick="window.location.href = 'candidatesPage.php';" style="margin-top:30px;margin-left:40px;">Candidates Page</button>
+        <button type="button" onclick="window.location.href = 'logout.php';">Log Out</button>
     </div>
 
 
