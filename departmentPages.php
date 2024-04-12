27cf22a9071a5fb79f4ff8c5994f3224c3aa13ea
tarie352 <92187204+tarie352@users.noreply.github.com>
2024-04-12 22:55:21 +0000

    Update departmentPages.php

--- departmentPages.php
+++ departmentPages.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Departments Page</title>
-    <link rel="stylesheet" href="../css/jobsPage.css" />
+    <link rel="stylesheet" href="jobsPage.css" />
 </head>
 
 <body>
@@ -15,7 +15,7 @@
     <main>
         <div class="candidate-listings">
             <?php
-            include("../functions/department_fuxn.php");
+            include("department_fuxn.php");
 
             // Close the database connection
             mysqli_close($con);
@@ -23,9 +23,9 @@
         </div>
     </main>
     <div class="button-container">
-        <button type="button" onclick="window.location.href = '../view/main.php';" style="margin-top:30px;margin-left:400px">Main Page</button>
-        <button type="button" onclick="window.location.href = '../view/departmentsInfo.php';">Department Information Page</button>
-        <button type="button" onclick="window.location.href = '../actions/logout.php';" style="height:30px; background-color:#007bff;color:white">Log Out</button>
+        <button type="button" onclick="window.location.href = 'main.php';" style="margin-top:30px;margin-left:400px">Main Page</button>
+        <button type="button" onclick="window.location.href = 'departmentsInfo.php';">Department Information Page</button>
+        <button type="button" onclick="window.location.href = 'logout.php';" style="height:30px; background-color:#007bff;color:white">Log Out</button>
     </div>
 
 </body>
