2469509c850d140ffee0771dd10e25bd2adbc0ba
tarie352 <92187204+tarie352@users.noreply.github.com>
2024-04-12 23:10:01 +0000

    Update modify_interviews.php

--- modify_interviews.php
+++ modify_interviews.php
@@ -5,29 +5,29 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Interviews Page</title>
-    <link rel="stylesheet" href="../css/register.css" />
+    <link rel="stylesheet" href="register.css" />
 </head>
 
 <body>
     <h1>Modify Interview Information</h1>
-    <?php include('../actions/interviewInfo.php'); ?>
+    <?php include('interviewInfo.php'); ?>
     <div class="container">
         <div class="container">
             <div class="registration-form">
-                <form action="../actions/edit_interviews.php" method="post">
+                <form action="edit_interviews.php" method="post">
                     <input type="hidden" name="interviewID" value="<?= $interview['interviewID'] ?>">
 
                     <div class="form-group" style="margin-bottom: 20px;">
                         <label for="candidateName">Candidate Name:</label><br />
                         <select id="candidateName" name="candidateName">
-                            <?php include("../functions/candidateNames_fuxn.php"); ?>
+                            <?php include("candidateNames_fuxn.php"); ?>
                         </select>
                     </div>
 
                     <div class="form-group" style="margin-bottom: 20px;">
                         <label for="jobID">Applied Job:</label><br />
                         <select id="jobID" name="jobID">
-                            <?php include('../functions/job_fxn.php'); ?>
+                            <?php include('job_fxn.php'); ?>
                         </select>
                     </div>
 
@@ -48,8 +48,8 @@
                     </select><br /><br />
 
                     <button type="submit" name="update_interview" id="update_interview">Submit Interview Information</button>
-                    <button type="button" onclick="window.location.href = '../view/main.php';" style="margin-top:30px;margin-left:30px;background-color:#007bff ;color:white">Main Page</button>
-                    <button type="button" onclick="window.location.href = '../view/interviewsInfo.php';" style="background-color:#007bff ;color:white">Interview Information Page</button>
+                    <button type="button" onclick="window.location.href = 'main.php';" style="margin-top:30px;margin-left:30px;background-color:#007bff ;color:white">Main Page</button>
+                    <button type="button" onclick="window.location.href = 'interviewsInfo.php';" style="background-color:#007bff ;color:white">Interview Information Page</button>
 
                 </form>
             </div>
