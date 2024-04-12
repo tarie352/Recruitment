039709e7d1c02bcce1b572ff0cafbbbf2ad3b705
tarie352 <92187204+tarie352@users.noreply.github.com>
2024-04-12 23:09:08 +0000

    Update modify_department.php

--- modify_department.php
+++ modify_department.php
@@ -5,20 +5,20 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Departments Page</title>
-    <link rel="stylesheet" href="../css/department.css" />
+    <link rel="stylesheet" href="department.css" />
 </head>
 
 <body>
     <header>
         <div class="header-links">
             <h2 class="heading">RECRUITMENT REVOLUTION: Departments</h2>
-            <a href="../view/postings.php" class="header-link">Back to Postings Page</a>
+            <a href="main" class="header-link">Back to Main Page</a>
 
         </div>
     </header>
 
     <div class="depart">
-        <form action="../actions/edit_department.php" method="POST">
+        <form action="edit_department.php" method="POST">
             <input type="hidden" id="deptID" name="deptID" value="<?= $department['departmentID'] ?>" /><br /><br />
 
             <label for="departmentName">Department Name:</label><br />
@@ -35,9 +35,9 @@
     </div>
 
     <div class="button-container">
-        <button type="button" onclick="window.location.href = '../view/main.php';" style="margin-top:30px;margin-left:400px">Main Page</button>
-        <button type="button" onclick="window.location.href = '../view/departmentsInfo.php';">Departments Information Page</button>
-        <button type="button" onclick="window.location.href = '../actions/logout.php';" style="height:30px; background-color:#007bff;color:white">Log Out</button>
+        <button type="button" onclick="window.location.href = 'main.php';" style="margin-top:30px;margin-left:400px">Main Page</button>
+        <button type="button" onclick="window.location.href = 'departmentsInfo.php';">Departments Information Page</button>
+        <button type="button" onclick="window.location.href = 'logout.php';" style="height:30px; background-color:#007bff;color:white">Log Out</button>
     </div>
 
 </body>
