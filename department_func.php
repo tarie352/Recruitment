817b698395a3403250c0df6d46b694c657587252
tarie352 <92187204+tarie352@users.noreply.github.com>
2024-04-12 22:56:01 +0000

    Update department_func.php

--- department_func.php
+++ department_func.php
@@ -1,6 +1,6 @@
 <?php
 // Include the database connection file
-include('../settings/connection.php');
+include('connection.php');
 
 $query = "SELECT departmentID, departmentName FROM department";
 
